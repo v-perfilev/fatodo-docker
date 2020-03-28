490e5eae87c41c7b3a4235255fd41b20a5c18f55
Vladimir Perfilev <persoff68@gmail.com>
2020-03-28 23:01:21 +0100

    add metrics

--- app.sh
+++ app.sh
@@ -4,6 +4,7 @@ if [[ $1 == "up" ]]; then
   ./app-network.sh up
   ./app-kafka.sh up -d
   ./app-elk.sh up -d
+  ./app-metrics.sh up -d
   ./app-zipkin.sh up -d
   ./app-mongo.sh up -d
   ./app-fatodo.sh up -d
@@ -12,6 +13,7 @@ fi
 if [[ $1 == "down" ]]; then
   ./app-kafka.sh down
   ./app-elk.sh down
+  ./app-metrics.sh down
   ./app-zipkin.sh down
   ./app-mongo.sh down
   ./app-fatodo.sh down
