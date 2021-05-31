a2b55d5fe6ec6dadc2f0f05508c0e15eaf2246d6
Vladimir Perfilev <persoff68@gmail.com>
2021-05-31 22:24:38 +0300

    edit profiles

--- app-web.sh
+++ app-web.sh
@@ -3,20 +3,15 @@
 if [[ $1 == "up" ]]; then
   ./network.sh up
   ./env-kafka.sh up -d
-  ./env-elk.sh up -d
-  ./env-metrics.sh up -d
-  ./env-zipkin.sh up -d
   ./env-redis.sh up -d
   ./env-mongo.sh up -d
+  ./env-mysql.sh up -d
   ./fatodo.sh up -d
   ./fatodo-web.sh up -d
 fi
 
 if [[ $1 == "down" ]]; then
   ./env-kafka.sh down
-  ./env-elk.sh down
-  ./env-metrics.sh down
-  ./env-zipkin.sh down
   ./env-redis.sh down
   ./env-mongo.sh down
   ./fatodo.sh down
@@ -25,6 +20,6 @@ if [[ $1 == "down" ]]; then
 fi
 
 if [[ $1 == "build" ]]; then
-  ./fatodo-dev.sh build
+  ./fatodo.sh build
   ./fatodo-web.sh build
 fi
