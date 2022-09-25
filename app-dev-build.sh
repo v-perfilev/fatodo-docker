91860d900a1e6a65ea7dd6d3ce8ec1b44352870e
Vladimir Perfilev <persoff68@gmail.com>
2022-09-25 18:13:14 +0200

    add buildx script

---
+++ app-dev-build.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+declare -a arr=("fatodo-auth-service"
+                "fatodo-chat-service"
+                "fatodo-comment-service"
+                "fatodo-config-service"
+                "fatodo-contact-service"
+                "fatodo-event-service"
+                "fatodo-gateway"
+                "fatodo-image-service"
+                "fatodo-item-service"
+                "fatodo-mail-service"
+                "fatodo-notification-service"
+                "fatodo-registry"
+                "fatodo-user-service"
+                "fatodo-ws-service"
+                )
+
+for i in "${arr[@]}"
+do
+  docker buildx build --load --tag "$i" --platform=linux/arm64 ../"$i"
+done
