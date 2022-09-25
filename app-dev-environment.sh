73397905e4924be330875fdd26e6a31a12a01519
Vladimir Perfilev <persoff68@gmail.com>
2022-09-25 18:16:26 +0200

    add env script

---
+++ app-dev-environment.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+if [[ $1 == "up" ]]; then
+  ./network.sh up
+  ./env-redis.sh up -d
+  ./env-mongo.sh up -d
+  ./env-mysql.sh up -d
+  ./env-rabbitmq.sh up -d
+fi
+
+if [[ $1 == "down" ]]; then
+  ./env-redis.sh down
+  ./env-mongo.sh down
+  ./env-mysql.sh down
+  ./env-rabbitmq.sh down
+  ./network.sh down
+fi
+
+if [[ $1 == "build" ]]; then
+  ./fatodo-dev.sh build
+fi
